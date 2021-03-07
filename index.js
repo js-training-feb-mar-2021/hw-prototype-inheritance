c4eebf55fddd7f3a45a2a4a81c2c8b45237e764b
Aleksandr Gordeev <gordeeffff@gmail.com>
2021-03-07 09:57:07 +0300

    feat: add order part

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function Hamburger(size, stuffing) {
     this._size = size;
     this._stuffing = stuffing;
+    this._name = this._size.name + " hamburger with " + this._stuffing.name;
 }
 
 Hamburger.SIZE_SMALL = {
@@ -46,16 +47,12 @@ Hamburger.prototype.calculateCalories = function() {
 
 var hamburger1 = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
 
-// console.log(hamburger1.getSize());
-// console.log(hamburger1.getStuffing());
-// console.log(hamburger1.calculatePrice());
-// console.log(hamburger1.calculateCalories());
-
-//Salad part
+//Salads part
 
 function Salad (type, weight) {
     this._type = type;
     this._weight = weight;
+    this._name = this._weight + " gram of " + this._type.name;
 }
 
 //Salads' price and calories are for each 100gram
@@ -87,19 +84,13 @@ Salad.prototype.calculateCalories = function() {
     return this._type.calories * this._weight / 100;
 }
 
-
 var caesar = new Salad(Salad.TYPE_CAESAR, 150);
-
-
-// console.log(caesar.getType());
-// console.log(caesar.getWeight());
-// console.log(caesar.calculatePrice());
-// console.log(caesar.calculateCalories());
 
 //Drinks part
 
 function Drink(type) {
     this._type = type;
+    this._name = this._type.name;
 }
 
 Drink.TYPE_COLA = {
@@ -128,6 +119,90 @@ Drink.prototype.calculateCalories = function() {
 
 var drink1 = new Drink(Drink.TYPE_COLA);
 
-// console.log(drink1.getType());
-// console.log(drink1.calculateCalories());
-// console.log(drink1.calculatePrice());
+//Order part
+function Order() {
+    this._order = [];
+    this._comfirmed = false;
+
+    for (var i = 0; i < arguments.length; i++) {
+        this._order.push(arguments[i]);
+    }
+}
+
+Order.prototype.showOrder = function() {
+    if(this._order.length < 1) {
+        console.log('Your order is empty');
+    } else {
+        // if console is needed
+        console.log("your order is:");
+        for (var i = 0; i < this._order.length; i++) {
+            console.log(this._order[i]._name + ": (price: " + this._order[i].calculatePrice() + ", calories: " +
+                +this._order[i].calculateCalories() +")");
+        }
+        // if object is needed
+        // return this._order;
+    }
+}
+
+Order.prototype.addItem = function(item) {
+    if(!this._comfirmed) {
+        this._order.push(item);
+        console.log(item._name + " was added to your order");
+    } else {
+        console.log("This order can't be changed");
+    }
+}
+
+Order.prototype.removeItem = function(item) {
+    var itemIndex = this._order.indexOf(item);
+    if(this._comfirmed === true) {
+        console.log("This order can't be changed");
+    } else if(itemIndex === -1) {
+        console.log("This order doesn't contain " + item._name);
+    } else if(itemIndex > -1) {
+        this._order.splice(itemIndex, 1);
+        console.log(item._name + " was removed from your order");
+    }
+}
+
+Order.prototype.confirmOrder = function() {
+    this._comfirmed = true;
+    console.log("The order is confirmed. You can't change it anymore");
+}
+
+Order.prototype.calculatePrice = function() {
+    var totalPrice = 0;
+    for (let i = 0; i < this._order.length; i++) {
+        var itemPrice = this._order[i].calculatePrice();
+        totalPrice += itemPrice;
+    }
+    return totalPrice;
+}
+
+Order.prototype.calculateCalories = function() {
+    var totalCalories = 0;
+    for (let i = 0; i < this._order.length; i++) {
+        var itemCalories = this._order[i].calculateCalories();
+        totalCalories += itemCalories;
+    }
+    return totalCalories;
+}
+
+var hamburger2 = new Hamburger(Hamburger.SIZE_BIG, Hamburger.STUFFING_POTATO);
+var drink2 = new Drink(Drink.TYPE_COFFEE);
+var olivier = new Salad(Salad.TYPE_OLIVIER, 200);
+
+var order1 = new Order(hamburger1, caesar, drink1,);
+
+order1.showOrder();
+console.log(order1.calculatePrice());
+console.log(order1.calculateCalories());
+order1.removeItem(hamburger2);
+order1.addItem(hamburger2);
+order1.addItem(olivier);
+order1.removeItem(hamburger1);
+order1.showOrder();
+console.log(order1.calculatePrice());
+console.log(order1.calculateCalories());
+order1.confirmOrder();
+order1.addItem(drink2);
